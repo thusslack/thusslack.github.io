9bb10b65417944b25d00776933511af002b4b386
Tino Hußlack <tino@picdrop.de>
2017-10-16 10:37:57 +0200

    adds upload progress

--- ff56upload/js/upload.js
+++ ff56upload/js/upload.js
@@ -3,6 +3,7 @@ function uploadFile(file) {
     var xhr = new XMLHttpRequest();
     var formData = new FormData();
     xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type","multipart/form-data");
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             // handle response
@@ -12,6 +13,11 @@ function uploadFile(file) {
             console.log(xhr.readyState, xhr.status, xhr.responseText);
         }
     };
+    xhr.addEventListener('progress', function(e) {
+        var done = e.position || e.loaded, total = e.totalSize || e.total;
+        console.log('xhr progress: ' + (Math.floor(done/total*1000)/10) + '%');
+    }, false);
+
     formData.append("upload_file", file);
     xhr.send(formData);
 }
@@ -19,6 +25,7 @@ function uploadFile(file) {
 var uploadfiles = document.querySelector('#uploads');
 uploadfiles.addEventListener('change', function () {
     var files = this.files;
+    console.log("files to upload:", files);
     for (var i = 0, len = files.length; i < len; i++) {
         uploadFile(this.files[i]); // call the function to upload the file
     }
