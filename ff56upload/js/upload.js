3dbdf7b782bdb084dc23ebc7588064ff330ee48c
Tino Hußlack <tino@picdrop.de>
2017-10-20 12:34:30 +0200

    added buttons for different upload test methods

--- ff56upload/js/upload.js
+++ ff56upload/js/upload.js
@@ -1,48 +1,135 @@
-function uploadFile(file) {
-    // add time loggers
-    console.time('load');
-    console.time('error');
-    console.time('abort');
-    console.time('upload');
+var logs = document.getElementById('logs'),
+    url = 'receiver.txt',
+    // set chunksize to 2 mb
+    chunkSize = 1024 * 1024 * 2;
 
-    var url = '/';
+/**
+ * ajax wrapper
+ *
+ * @param payload
+ * @param filename
+ */
+function sendData(payload, filename) {
+    // add time logger
+    var tu = performance.now();
+
+    // set xhr method
     var xhr;
-    var chunkSize = 1024 * 1024 * 2;
+    if (window.XMLHttpRequest) xhr = new XMLHttpRequest();
+    else if (window.ActiveXObject) xhr = new ActiveXObject("Microsoft.XMLHTTP");
 
-    if(window.XMLHttpRequest)       xhr = new XMLHttpRequest();
-    else if(window.ActiveXObject)   xhr = new ActiveXObject("Microsoft.XMLHTTP");
-    var sliceMethod = file.slice ? 'slice' : (file.webkitSlice ? 'webkitSlice' : (file.mozSlice ? 'mozSlice' : false));
-
-
-
-    xhr.open("POST", url, true);
-    //xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-    xhr.setRequestHeader("X_FILENAME", encodeURIComponent(file.name));
+    // add listeners
     xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            // handle response
-            console.log('request completed successful', xhr.readyState, xhr.status, xhr.responseText);
-        } else {
-            // handle error
-            console.log('request error', xhr.readyState, xhr.status, xhr.responseText);
+        if (xhr.readyState === 1) {
+            logs.value += 'Connection opened: ' + (performance.now() - tu) + ' \n';
         }
-        console.timeEnd('upload');
+        if (xhr.readyState === 4) {
+            console.log('request completed successful', xhr.readyState, xhr.status);
+            logs.value += 'Upload complete: ' + (performance.now() - tu) + ' \n\n';
+        }
     };
-    xhr.addEventListener('abort', function(e) { console.timeEnd('abort'); });
-    xhr.addEventListener('error', function(e) { console.timeEnd('error'); });
-    xhr.addEventListener('load', function(e) {  console.timeEnd('load'); });
-    xhr.addEventListener('progress', function(e) {
+    xhr.addEventListener('abort', function (e) {
+        logs.value += 'Abort: ' + (performance.now() - tu) + ' \n';
+    });
+    xhr.addEventListener('error', function (e) {
+        logs.value += 'Error: ' + (performance.now() - tu) + ' \n';
+    });
+    xhr.addEventListener('progress', function (e) {
         var done = e.position || e.loaded, total = e.totalSize || e.total;
-        console.log('xhr progress: ' + (Math.floor(done/total*1000)/10) + '%');
+        var msg = 'xhr progress: ' + (Math.floor(done / total * 1000) / 10) + '%';
+        logs.value += msg + '\n';
     }, false);
 
-    // send first chunk of chunksize
-    xhr.send(file[sliceMethod](chunkSize));
+    // open connection
+    xhr.open("POST", url, true);
+    if (filename) {
+        xhr.setRequestHeader("X_FILENAME", encodeURIComponent(filename));
+    }
+    // send payload
+    xhr.send(payload);
 }
 
-var uploadfiles = document.querySelector('#uploads');
-uploadfiles.addEventListener('change', function () {
-    var file = this.files[0];
-    console.log("file to upload:", file);
-    uploadFile(file); // call the function to upload the file
+function uploadFile(file) {
+    sendData(file);
+}
+
+function chunkInput(file) {
+    var sliceMethod = file.slice ? 'slice' : (file.webkitSlice ? 'webkitSlice' : (file.mozSlice ? 'mozSlice' : false));
+    return file[sliceMethod](0, chunkSize);
+}
+
+/**
+ * prepares a file chunk to upload
+ *
+ * @param file
+ */
+function uploadFileChunked(file) {
+    // set slice method if file available
+    if (file) {
+        sendData(chunkInput(file));
+    }
+}
+
+/**
+ * uses suggested filereader bytearray conversion workaround to generate a blob for upload
+ *
+ * @param file
+ */
+function uploadFileChunkedByteArray(file) {
+    var reader = new FileReader();
+    reader.addEventListener("load", function (e) {
+        var payload = new Blob([e.target.result]);
+        console.log("payload", payload);
+        sendData(payload, file.name);
+    }, false);
+    reader.readAsArrayBuffer(chunkInput(file));
+}
+
+/**
+ * uses suggested filereader bytearray conversion workaround to generate a blob for upload
+ *
+ * @param file
+ */
+function uploadFileByteArray(file) {
+    var reader = new FileReader();
+    reader.addEventListener("load", function (e) {
+        var payload = new Blob([e.target.result]);
+        console.log("payload", payload);
+        sendData(payload, file.name);
+    }, false);
+    reader.readAsArrayBuffer(file);
+}
+
+// setup ajax buttons
+document.getElementById('upload').addEventListener('click', function () {
+    var file = document.querySelector('#uploads').files[0];
+    logs.value += 'Sending full file ' + file.name + '\n';
+    logs.value += '--- \n';
+    uploadFile(file, file.name);
 }, false);
+
+document.getElementById('upload_c').addEventListener('click', function () {
+    var file = document.querySelector('#uploads').files[0];
+    logs.value += 'Sending chunked file ' + file.name + '\n';
+    logs.value += '--- \n';
+    uploadFileChunked(file, file.name);
+}, false);
+
+document.getElementById('upload_ba').addEventListener('click', function () {
+    var file = document.querySelector('#uploads').files[0];
+    logs.value += 'Sending byte array file ' + file.name + '\n';
+    logs.value += '--- \n';
+    uploadFileByteArray(file);
+}, false);
+document.getElementById('upload_bac').addEventListener('click', function () {
+    var file = document.querySelector('#uploads').files[0];
+    logs.value += 'Sending byte array chunked file ' + file.name + '\n';
+    logs.value += '--- \n';
+    uploadFileChunkedByteArray(file);
+}, false);
+
+// test empty request timing
+logs.value += 'Sending empty request \n';
+logs.value += '--- \n';
+uploadFile('');
+
